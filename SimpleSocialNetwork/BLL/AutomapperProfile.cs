5c248016318cecfc876185b7fba349836a6695bd
bsodss <bsods@gmail.com>
2021-08-29 15:27:19 +0300

    created BLL layaer

---
+++ SimpleSocialNetwork/BLL/AutomapperProfile.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using AutoMapper;
+
+namespace BLL
+{
+    public class AutomapperProfile : Profile
+    {
+        public AutomapperProfile()
+        {
+
+        }
+    }
+}
---
+++ SimpleSocialNetwork/BLL/Interfaces/ICrud.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BLL.Interfaces
+{
+
+    public interface ICrud<TModel> where TModel : class
+    {
+        IEnumerable<TModel> GetAll();
+
+        Task<TModel> GetByIdAsync(int id);
+
+        Task AddAsync(TModel model);
+
+        Task UpdateAsync(TModel model);
+
+        Task DeleteByIdAsync(int modelId);
+    }
+
+
+}
