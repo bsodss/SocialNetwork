4d389e06ed181d4b78ca1e45924bad6e1712f7c0
bsodss <bsods@gmail.com>
2021-08-31 22:21:02 +0300

    added filter search model

---
+++ SimpleSocialNetwork/BLL/Models/FilterSearchModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BLL.Models
+{
+    public class FilterSearchModel
+    {
+        public string City { get; set; }
+        public int AgeFrom { get; set; }
+        public int AgeTo { get; set; }
+        public bool IsMale { get; set; }
+
+        public bool WithPhoto { get; set; }
+
+    }
+}
---
+++ SimpleSocialNetwork/BLL/Validation/SocialNetworkException.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BLL.Validation
+{
+    public class SocialNetworkException:Exception
+    {
+        public string Property { get; set; }          
+        public SocialNetworkException(string message):base(message)
+        {
+                
+        }
+
+        public SocialNetworkException(string message, string property): base(message)
+        {
+            Property = property;
+        }
+    }
+}
